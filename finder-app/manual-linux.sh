9ae60b0476be276e02b654291505523977d56dcf
EricTranAvnet <eric.tran@avnet.eu>
2025-08-25 09:14:59 +0200

    Adding variable busybox

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,7 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+BUSYBOX=https://github.com/mirror/busybox
 
 if [ $# -lt 1 ]
 then
@@ -82,7 +83,8 @@ mkdir -p ${OUTDIR}/rootfs/bin \
 if [ ! -d "${OUTDIR}/busybox" ]
 then
     cd ${OUTDIR}
-    git clone https://github.com/mirror/busybox
+    echo "Cloning busybox with source : ${BUSYBOX}"
+    git clone ${BUSYBOX}
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
